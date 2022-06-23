7fecc3f43eb593d721b733340c0381c9d0439cfa
michael golden <michaelwilliamgolden@gmail.com>
2022-06-22 17:27:48 -0700

    css

---
+++ map_nft_frontend/components/Description.js
@@ -0,0 +1,48 @@
+export default function Description() {
+    return (
+<div className="intro">
+           
+    <div className="about">
+        <h1>welcome to the new world.</h1>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,200,1,0" />
+            <span className="material-symbols-outlined" >bolt</span>
+         </div>
+          <div>
+          <img className="align-right" alt=" "  draggable= {false}src="https://www.nps.gov/npgallery/GetAsset/81B0006B-B460-450F-81EF-1C88405F9CDF/proxy/hires" width="100" height="100" />
+
+          </div>
+<div className="descr">
+<h1> Description 🗺️</h1>
+<table><tr><td><span className="material-symbols-outlined">star</span></td><td> This is the beginning of the world´s first geographic-based system of NFTs. </td></tr></table>
+<table><tr><td><span className="material-symbols-outlined">star</span></td><td> Nosotro is about us, the individuals part of something bigger than ourselves. envision a future where web3 creators can have a digital gathering place of their own.</td></tr></table>
+<table><tr><td><span className="material-symbols-outlined">star</span></td><td> The modern world we see today is a result of preceding community efforts. the new future is created from the innovative communities of today. </td></tr></table> 
+
+  <table><tr><td><span className="material-symbols-outlined">star</span></td><td> ´Uniformitarianism´ is a key term which emphasizes that the processes that shaped the past are the same as those operating today. </td></tr></table>
+  <table><tr><td><span className="material-symbols-outlined">star</span></td><td> while the startup is in its beta phase, we have no plans to announce our twitter, discord, or roadmap until the minting occurs. Mark your calendars: Counties will be available to mint on July 4th, 2022.  </td></tr></table> 
+  
+</div>
+<div>
+     <img className="align-left" alt=" " draggable= {false} src="https://www.edenprojectcommunities.com/sites/default/files/styles/step_image/public/Community%20Art%20mural%20in%20Tower%20Hamlets%2C%20Eden%20Project%20Communities.jpg?itok=7nAuoJVu" width="100" height="100" />
+
+          </div>
+          
+<div className="descr2">
+
+  <h1>Instructions 🗺️</h1>
+  <table><tr><td><span className="material-symbols-outlined">hide_source</span></td><td> The canvas is given to you, but only you can find a way to paint it. </td></tr></table>
+  <table><tr><td><span className="material-symbols-outlined">hide_source</span></td><td> Here, you can share your creations with everyone.</td></tr></table>
+  <table><tr><td><span className="material-symbols-outlined">hide_source</span></td><td> When you own at least one NFT [a county or state], you can create your art on a canvas to be displayed on the map, and gain access to the discord which is exclusive for holders.  </td></tr></table> 
+
+</div>
+
+<div className="layout">
+    
+      <footer>
+        <p>© 2022 nosotro</p>
+       
+      </footer>
+    </div>
+   </div>
+
+    )
+  }
--- map_nft_frontend/pages/index.js
+++ map_nft_frontend/pages/index.js
@@ -4,6 +4,7 @@ import MapElement from "../components/MapElement"
 import GetAllNFT from "../components/county/GetAllNFT"
 import MintNFT from "../components/county/MintNFT"
 import GetUserNFT from "../components/county/GetUserNFT"
+import Description from "../components/Description"
 
 export default function Home() {
   const { isWeb3Enabled, account } = useMoralis()
@@ -23,6 +24,11 @@ export default function Home() {
           <GetAllNFT />
         </div>
       )}
+<div className="flex flex-col items-center justify-center">
+         <Description />
+            </div>
+
+
     </div>
   )
 }
